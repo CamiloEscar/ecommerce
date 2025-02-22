4175f8a9809e27cda6f53e8682df82e71eb0bd55
CamiloEscar <camiloescar1995@gmail.com>
2025-02-22 17:01:07 -0300

    implementacion de plantilla html a la pagina principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+declare var $: any;
+declare function HOMEINIT([]): any;
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ecommerce';
+  constructor() {
+    setTimeout(() => {
+      HOMEINIT($);
+    }, 50);
+  }
 }
