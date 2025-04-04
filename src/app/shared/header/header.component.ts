9c995c172908534fbe3f1cc3ed66532e40b743d7
CamiloEscar <camiloescar1995@gmail.com>
2025-04-04 18:56:41 -0300

    Header: Menu y submenu de categorias

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component } from '@angular/core';
+import { HomeService } from '../../pages/home/service/home.service';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
 
+  categories_menus: any = [];
+
+
+  constructor(public homeService: HomeService) {
+    afterNextRender(() => {
+      this.homeService.home().subscribe((resp: any) => {
+        console.log(resp);
+        this.categories_menus = resp.categories_menus;
+      });
+    });
+  }
+  getIconMenu(MENU_CAT:any){
+    var miDiv: any = document.getElementById('icon-' + MENU_CAT.id);
+    miDiv.innerHTML = MENU_CAT.icon;
+    return '';
+  }
 }
