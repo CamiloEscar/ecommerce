ac2fdf4b4915cd41510b13bf863f2fbff0bbfeec
CamiloEscar <camiloescar1995@gmail.com>
2025-04-18 01:31:49 -0300

    Formulario de edicion del cliente

--- src/app/pages/view-auth/profile-client/edit-profile-client/edit-profile-client.component.ts
+++ src/app/pages/view-auth/profile-client/edit-profile-client/edit-profile-client.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { ProfileClientService } from '../service/profile-client.service';
+import { ToastrService } from 'ngx-toastr';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-edit-profile-client',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './edit-profile-client.component.html',
   styleUrl: './edit-profile-client.component.css'
 })
 export class EditProfileClientComponent {
 
+  name: string = '';
+  surname: string = '';
+  email: string = '';
+  phone: string = '';
+  bio: string = '';
+  fb: string = '';
+  // ig: string = '';
+  sexo: string = '';
+  address_city: string = '';
+
+  constructor(
+    public profileClient: ProfileClientService,
+    public toastr: ToastrService,
+  ) {
+    this.profileClient.showUser().subscribe((resp:any) => {
+      console.log(resp)
+      this.name = resp.name
+      this.surname = resp.surname
+      this.email = resp.email
+      this.phone = resp.phone
+      this.bio = resp.bio
+      this.fb = resp.fb
+      // this.ig = resp.ig
+      this.sexo = resp.sexo
+      this.address_city = resp.address_city
+    })
+  }
+
+  updateUser(){
+  }
 }
