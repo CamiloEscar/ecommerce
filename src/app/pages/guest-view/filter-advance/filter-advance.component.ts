306380dd2307af0644aae7913e2d32729c750432
CamiloEscar <camiloescar1995@gmail.com>
2025-04-22 17:57:39 -0300

    Filtro por campaña de descuento

--- src/app/pages/guest-view/filter-advance/filter-advance.component.ts
+++ src/app/pages/guest-view/filter-advance/filter-advance.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { afterNextRender, ChangeDetectorRef, Component } from '@angular/core';
 import { HomeService } from '../../home/service/home.service';
 import { CookieService } from 'ngx-cookie-service';
 import { CommonModule } from '@angular/common';
@@ -41,6 +41,8 @@ export class FilterAdvanceComponent {
   min_price: number = 0;
   max_price: number = 0;
 
+  options_aditional:any = [];
+
   constructor(
     public homeService: HomeService,
     public cookieService: CookieService,
@@ -69,23 +71,41 @@ export class FilterAdvanceComponent {
     // Set currency FIRST before loading any data
     this.currency = this.cookieService.get("currency") || 'ARS';
 
-    $("#slider-range").slider({
-			range: true,
-			min: 0,
-			max: 40000,
-			values: [10, 10000],
-			slide: (event:any, ui:any) => {
-				$("#amount").val(this.currency+ " " + ui.values[0] + " - "+this.currency+ " " + ui.values[1]);
-        this.min_price = ui.values[0];
-        this.max_price = ui.values[1];
-			},stop: () => {
-        this.filterAdvanceProduct();
+    if(typeof $ !== 'undefined'){
+      $("#slider-range").slider({
+            range: true,
+            min: 0,
+            max: 40000,
+            values: [10, 10000],
+            slide: (event:any, ui:any) => {
+              $("#amount").val(this.currency+ " " + ui.values[0] + " - "+this.currency+ " " + ui.values[1]);
+              this.min_price = ui.values[0];
+              this.max_price = ui.values[1];
+            },stop: () => {
+              this.filterAdvanceProduct();
+            }
+          });
+          $("#amount").val(this.currency+ " " + $("#slider-range").slider("values", 0) +
+            " - "+this.currency+ " " + $("#slider-range").slider("values", 1));
       }
-		});
-		$("#amount").val(this.currency+ " " + $("#slider-range").slider("values", 0) +
-			" - "+this.currency+ " " + $("#slider-range").slider("values", 1));
     };
 
+    addOptionAditional(option:string){
+    //chequeamos si la categoria ya fue seleccionada
+    let INDEX = this.options_aditional.findIndex((item:any) => item == option);
+    //SI YA EXISTE
+    if(INDEX != -1){
+      //la eliminamos de la lista de seleccionados
+      this.options_aditional.splice(INDEX, 1)
+    } else {
+      //si no fue seleccionada la agregamos
+      this.options_aditional.push(option);
+    }
+    // console.log(this.categories_selected)
+    //cada vez que seleccionamos una categoria llamamos al servicio para filtrar los product
+    this.filterAdvanceProduct();
+
+    }
 
     addCategorie(categorie:any){
 
@@ -138,8 +158,6 @@ export class FilterAdvanceComponent {
       this.filterAdvanceProduct();
     }
 
-
-
     filterAdvanceProduct(){
 
       let data = {
@@ -149,6 +167,7 @@ export class FilterAdvanceComponent {
         min_price: this.min_price,
         max_price: this.max_price,
         currency: this.currency,
+        options_aditional: this.options_aditional,
       }
       this.homeService.filterAdvanceProduct(data).subscribe((resp:any) => {
         console.log(resp);
@@ -156,166 +175,166 @@ export class FilterAdvanceComponent {
       })
     }
 
-  getTotalCurrency(PRODUCTS: any): number {
-    // Add null checks
-    if (!PRODUCTS) {
-      return 0;
-    }
-
-
-    let price: number;
-
-    if (this.currency == 'ARS') {
-      if (!PRODUCTS.price_ars) {
-        return 0;
-      }
-      price = PRODUCTS.price_ars;
-    } else {
-      if (!PRODUCTS.price_usd) {
-        return 0;
-      }
-      price = PRODUCTS.price_usd;
-    }
-
-    // Always return a number with 2 decimal places
-    return parseFloat(price.toFixed(2));
-  }
-
-  addCart(PRODUCT: any): void {
-    if (!this.cartService.authService.user) {
-      this.toastr.error('Error', 'Ingrese a la tienda');
-      this.router.navigateByUrl("/login");
-      return;
-    }
-
-    if (PRODUCT.variations && PRODUCT.variations.length > 0) {
-      $("#producQuickViewModal").modal("show");
-      this.openDetailProduct(PRODUCT);
-      return;
-    }
-
-    let discount_g = null;
-
-    if (PRODUCT.discount_g) {
-      discount_g = PRODUCT.discount_g;
-    }
-
-    let data = {
-      product_id: PRODUCT.id,
-      type_discount: discount_g ? discount_g.type_discount : null,
-      discount: discount_g ? discount_g.discount : null,
-      type_campaing: discount_g ? discount_g.type_campaing : null,
-      code_cupon: null,
-      code_discount: discount_g ? discount_g.code : null,
-      product_variation_id: null,
-      quantity: 1,
-      price_unit: this.currency == 'ARS' ? PRODUCT.price_ars : PRODUCT.price_usd,
-      subtotal: this.getTotalPriceProduct(PRODUCT),
-      total: this.getTotalPriceProduct(PRODUCT) * 1,
-      currency: this.currency,
-    };
-
-    this.cartService.registerCart(data).subscribe(
-      (resp: any) => {
-        if (resp.message == 403) {
-          this.toastr.error('Error', resp.message_text);
-        } else {
-          this.cartService.changeCart(resp.cart);
-          this.toastr.success('Exito', 'El producto se agrego al carrito de compra');
-        }
-      },
-      (err) => {
-        console.error(err);
-      }
-    );
-  }
-
-  openDetailProduct(PRODUCT: any, DISCOUNT_FLASH: any = null): void {
-    // First reset the variation_selected
-    this.product_selected = null;
-    this.variation_selected = null;
-
-    // Then set the new product
-    setTimeout(() => {
-      this.product_selected = PRODUCT;
-
-      setTimeout(() => {
-        if (DISCOUNT_FLASH) {
-          this.product_selected.discount_g = DISCOUNT_FLASH;
-        }
-        this.cdr.detectChanges(); // Force change detection after updating product
-      }, 25);
-    }, 100);
-  }
-
-  getNewTotal(PRODUCTS: any, DISCOUNT_FLASH_P: any): number {
-    // Add null checks
-    if (!PRODUCTS || !DISCOUNT_FLASH_P) {
-      return 0;
-    }
-
-    let result: number;
-
-    if (this.currency == 'ARS') {
-      if (!PRODUCTS.price_ars) {
+    getTotalCurrency(PRODUCTS: any): number {
+      // Add null checks
+      if (!PRODUCTS) {
         return 0;
       }
 
-      if (DISCOUNT_FLASH_P.type_discount == 1) { //% de descuento
-        result = PRODUCTS.price_ars - (PRODUCTS.price_ars * (DISCOUNT_FLASH_P.discount * 0.01));
-      } else { //monto fijo /-pesos -dolares
-        result = PRODUCTS.price_ars - DISCOUNT_FLASH_P.discount;
-      }
-    } else {
-      if (!PRODUCTS.price_usd) {
-        return 0;
-      }
 
-      if (DISCOUNT_FLASH_P.type_discount == 1) { //% de descuento
-        result = PRODUCTS.price_usd - (PRODUCTS.price_usd * (DISCOUNT_FLASH_P.discount * 0.01));
-      } else { //monto fijo /-pesos -dolares
-        result = PRODUCTS.price_usd - DISCOUNT_FLASH_P.discount;
-      }
-    }
+      let price: number;
 
-    // Always return a number with 2 decimal places
-    return parseFloat(result.toFixed(2));
-  }
-
-  getTotalPriceProduct(PRODUCTS: any): number {
-    // Add null checks
-    if (!PRODUCTS) {
-      return 0;
-    }
-
-    let result: number;
-
-    // primero chequeamos que hay un descuento en el producto
-    if (PRODUCTS.discount_g) {
-      result = this.getNewTotal(PRODUCTS, PRODUCTS.discount_g);
-    }
-    // luego chequeamos si tiene el descuento flash
-    else if (this.DISCOUNT_FLASH && this.PRODUCTS &&
-             this.PRODUCTS.includes &&
-             this.PRODUCTS.includes(PRODUCTS)) {
-      result = this.getNewTotal(PRODUCTS, this.DISCOUNT_FLASH);
-    }
-    // precio comun
-    else {
-      if (this.currency == "ARS") {
+      if (this.currency == 'ARS') {
         if (!PRODUCTS.price_ars) {
           return 0;
         }
-        result = parseFloat(PRODUCTS.price_ars.toFixed(2));
+        price = PRODUCTS.price_ars;
       } else {
         if (!PRODUCTS.price_usd) {
           return 0;
         }
-        result = parseFloat(PRODUCTS.price_usd.toFixed(2));
+        price = PRODUCTS.price_usd;
       }
+
+      // Always return a number with 2 decimal places
+      return parseFloat(price.toFixed(2));
     }
 
-    return result;
-  }
+    addCart(PRODUCT: any): void {
+      if (!this.cartService.authService.user) {
+        this.toastr.error('Error', 'Ingrese a la tienda');
+        this.router.navigateByUrl("/login");
+        return;
+      }
+
+      if (PRODUCT.variations && PRODUCT.variations.length > 0) {
+        $("#producQuickViewModal").modal("show");
+        this.openDetailProduct(PRODUCT);
+        return;
+      }
+
+      let discount_g = null;
+
+      if (PRODUCT.discount_g) {
+        discount_g = PRODUCT.discount_g;
+      }
+
+      let data = {
+        product_id: PRODUCT.id,
+        type_discount: discount_g ? discount_g.type_discount : null,
+        discount: discount_g ? discount_g.discount : null,
+        type_campaing: discount_g ? discount_g.type_campaing : null,
+        code_cupon: null,
+        code_discount: discount_g ? discount_g.code : null,
+        product_variation_id: null,
+        quantity: 1,
+        price_unit: this.currency == 'ARS' ? PRODUCT.price_ars : PRODUCT.price_usd,
+        subtotal: this.getTotalPriceProduct(PRODUCT),
+        total: this.getTotalPriceProduct(PRODUCT) * 1,
+        currency: this.currency,
+      };
+
+      this.cartService.registerCart(data).subscribe(
+        (resp: any) => {
+          if (resp.message == 403) {
+            this.toastr.error('Error', resp.message_text);
+          } else {
+            this.cartService.changeCart(resp.cart);
+            this.toastr.success('Exito', 'El producto se agrego al carrito de compra');
+          }
+        },
+        (err) => {
+          console.error(err);
+        }
+      );
+    }
+
+    openDetailProduct(PRODUCT: any, DISCOUNT_FLASH: any = null): void {
+      // First reset the variation_selected
+      this.product_selected = null;
+      this.variation_selected = null;
+
+      // Then set the new product
+      setTimeout(() => {
+        this.product_selected = PRODUCT;
+
+        setTimeout(() => {
+          if (DISCOUNT_FLASH) {
+            this.product_selected.discount_g = DISCOUNT_FLASH;
+          }
+          this.cdr.detectChanges(); // Force change detection after updating product
+        }, 25);
+      }, 100);
+    }
+
+    getNewTotal(PRODUCTS: any, DISCOUNT_FLASH_P: any): number {
+      // Add null checks
+      if (!PRODUCTS || !DISCOUNT_FLASH_P) {
+        return 0;
+      }
+
+      let result: number;
+
+      if (this.currency == 'ARS') {
+        if (!PRODUCTS.price_ars) {
+          return 0;
+        }
+
+        if (DISCOUNT_FLASH_P.type_discount == 1) { //% de descuento
+          result = PRODUCTS.price_ars - (PRODUCTS.price_ars * (DISCOUNT_FLASH_P.discount * 0.01));
+        } else { //monto fijo /-pesos -dolares
+          result = PRODUCTS.price_ars - DISCOUNT_FLASH_P.discount;
+        }
+      } else {
+        if (!PRODUCTS.price_usd) {
+          return 0;
+        }
+
+        if (DISCOUNT_FLASH_P.type_discount == 1) { //% de descuento
+          result = PRODUCTS.price_usd - (PRODUCTS.price_usd * (DISCOUNT_FLASH_P.discount * 0.01));
+        } else { //monto fijo /-pesos -dolares
+          result = PRODUCTS.price_usd - DISCOUNT_FLASH_P.discount;
+        }
+      }
+
+      // Always return a number with 2 decimal places
+      return parseFloat(result.toFixed(2));
+    }
+
+    getTotalPriceProduct(PRODUCTS: any): number {
+      // Add null checks
+      if (!PRODUCTS) {
+        return 0;
+      }
+
+      let result: number;
+
+      // primero chequeamos que hay un descuento en el producto
+      if (PRODUCTS.discount_g) {
+        result = this.getNewTotal(PRODUCTS, PRODUCTS.discount_g);
+      }
+      // luego chequeamos si tiene el descuento flash
+      else if (this.DISCOUNT_FLASH && this.PRODUCTS &&
+              this.PRODUCTS.includes &&
+              this.PRODUCTS.includes(PRODUCTS)) {
+        result = this.getNewTotal(PRODUCTS, this.DISCOUNT_FLASH);
+      }
+      // precio comun
+      else {
+        if (this.currency == "ARS") {
+          if (!PRODUCTS.price_ars) {
+            return 0;
+          }
+          result = parseFloat(PRODUCTS.price_ars.toFixed(2));
+        } else {
+          if (!PRODUCTS.price_usd) {
+            return 0;
+          }
+          result = parseFloat(PRODUCTS.price_usd.toFixed(2));
+        }
+      }
+
+      return result;
+    }
 
 }
